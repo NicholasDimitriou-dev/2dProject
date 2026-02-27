5c553a5bd4e8059fd9599370289fc401ca1555fa
Nicholas Dimitriou <skeleynick@gmail.com>
2026-02-26 20:29:32 -0800

    setup

--- Assets/2D Project/Scripts/Player.cs
+++ Assets/2D Project/Scripts/Player.cs
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
         {
             GameObject shot = Instantiate(bulletPrefab, shootOffsetTransform.position, Quaternion.identity);
             Debug.Log("Bang!");
-            
+            GetComponent<Animator>().SetTrigger("hasShot");
             Destroy(shot, 3f);
             // todo - destroy the bullet after 3 seconds
             // todo - trigger shoot animation
